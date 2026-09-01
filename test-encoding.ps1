e53bcded00ad9ff88df58a529b2c1ff54e5d74d6
Holwan <holmanmontesmontes57@gmail.com>
2026-09-01 16:03:53 -0500

    eliminacion de archivos no usados

--- test-encoding.ps1
+++
@@ -1,4 +0,0 @@
-$content = Get-Content -Path "Pages\about.html" -Raw -Encoding UTF8
-$bytes = [System.Text.Encoding]::GetEncoding("windows-1252").GetBytes($content)
-$fixed = [System.Text.Encoding]::UTF8.GetString($bytes)
-$fixed | Select-String "LOGROS DESBLOQUEADOS"
--- test.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('dist/index.html', 'utf8');
-const start = content.indexOf('<section class="hero"');
-const end = content.indexOf('</section>', start) + 10;
-console.log(content.substring(start, end));
